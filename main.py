0625baea700a5197ac7e7dff3dcc23505c84e286
jwklau <nuffah@gmail.com>
2022-08-31 21:06:33 +0100

    Add DATABASE_URL environment variable, add psycopg2-binary package, update requirements.txt

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
